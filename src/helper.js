54a2d592f497efc95b0862834ce7232bcf6283a5
Dave W <dviper@gmail.com>
2017-06-26 15:11:58 -0600

    start building out DistrictRepository class

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,20 @@
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+
+
 export default class DistrictRepository {
+  constructor(kinderData) {
+    this.data = kinderData.reduce((acc, item) => {
+      if (!acc[item.Location]) {
+        acc[item.Location] = Object.keys(item).reduce((acc, node) => {
+          if (!acc[node.TimeFrame]) {
+            acc[node.TimeFrame] = item.TimeFrame
+          }
+          return acc;
+        },{})
+      }
+      return acc
+    },{})
+    console.log(this.data);
+  }
 
 }
