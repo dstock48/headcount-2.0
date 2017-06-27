cec6c67d3b4ac5e1380cf845984ff7e8c06a98a3
Dave W <dviper@gmail.com>
2017-06-27 17:48:16 -0600

    add styling to app

--- src/DistrictCard.js
+++ src/DistrictCard.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 const DistrictCard = ( {distData}) => {
   let yearData = Object.keys(distData.data).map((year) => {
-    return <p className="district-data" key={Math.random()*year}>{year}: {distData.data[year]}</p>
+    let accentClass;
+    if (distData.data[year] > 0.5) {
+      accentClass = 'high'
+    } else {
+      accentClass = 'low'
+    }
+    return <p className="district-data" key={Math.random()*year}><span className="year">{year}:</span> <span className={accentClass}>{distData.data[year]}</span></p>
   })
   return (
   <div className="district-card">
     <h1 className="district-location">{distData.location}</h1>
-    {yearData}
+    <div className="yearData">
+      {yearData}
+    </div>
 
   </div>
 )}
--- src/DistrictList.js
+++ src/DistrictList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import DistrictCard from './DistrictCard'
 import PropTypes, {array, shape, arrayOf} from 'prop-types'
+require('./DistrictList.css');
 
 const DistrictList = ( { districts } ) => {
   const districtsArray = districts.map(district => <DistrictCard key={district.location} distData={district}/>);
   return (
-    <div>
+    <div className="district-list">
       {districtsArray}
     </div>
   )
