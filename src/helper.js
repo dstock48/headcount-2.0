a057927dbd976a361cbee5770871335c82ea6004
Dave W <dviper@gmail.com>
2017-06-27 13:52:38 -0600

    complete iteration1-part2 tests

--- src/helper.js
+++ src/helper.js
@@ -15,13 +15,16 @@ export default class DistrictRepository {
     this.data = this.normalizedData.reduce((acc, item) => {
       const roundedData = () => {
         if (typeof item.data === 'number') {
-        return Math.round(item.data*1000)/1000
+        return Math.round(item.data * 1000) / 1000
       } else {
         return 0
       }
     }
       if (!acc[item.location]) {
-        acc[item.location] = {location: item.location, data: {[item.timeFrame]: roundedData()}}
+        acc[item.location] = {
+          location: item.location,
+          data: {[item.timeFrame]: roundedData()}
+        }
 
           return acc
       } else {
@@ -33,7 +36,6 @@ export default class DistrictRepository {
     },{})
   }
 
-
   findByName(searchTerm) {
     if (arguments.length === 0) return undefined
 
@@ -41,8 +43,22 @@ export default class DistrictRepository {
     let location = Object.keys(this.data).find(element => {
       return element.toLowerCase() === searchTermLower
     })
-
     return this.data[location]
+  }
+
+  findAllMatches(searchTerm) {
+    let dataKeys = Object.keys(this.data)
+    const result = dataKeys.reduce((acc, key) => {
+
+      if (arguments.length === 0) {
+        acc.push(this.data[key])
+      } else if (key.toLowerCase().split(' ').includes(searchTerm.toLowerCase())) {
+        acc.push(this.data[key])
+      }
+      return acc
+    },[])
+
+    return result
   }
 
 }
